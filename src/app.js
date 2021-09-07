e1440b161f27feb99c03a6f3ed4491e147e2aec9
aircity <aircity@drrr.us>
2021-09-07 21:34:27 +0800

    commit

--- src/app.js
+++ src/app.js
@@ -77,9 +77,9 @@ class App {
     const talentPage = $(`
         <div id="main">
             <div class="head" style="font-size: 1.6rem">天赋抽卡</div>
-            <button id="random" class="mainbtn" style="top: 50%;">10连抽！</button>
+            <button id="random" class="mainbtn" style="top: 50%;">天胡自摸！</button>
             <ul id="talents" class="selectlist"></ul>
-            <button id="next" class="mainbtn" style="top:auto; bottom:0.1em">请选择3个</button>
+            <button id="next" class="mainbtn" style="top:auto; bottom:0.1em">请选择</button>
         </div>
         `);
 
